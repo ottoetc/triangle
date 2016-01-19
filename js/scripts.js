ddc44d81a8cc2f8d2ca14e731764c9482c78c8d3
Nathan Otto and Schuyler Klaassen <schuyler.m.k@gmail.com>
2016-01-19 15:09:39 -0800

    add interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
+var kind;
 var triangle = function(leg1,leg2,leg3) {
   //triangle
   if ((leg1 + leg2 > leg3) && (leg1 +leg3 > leg2) && (leg2 + leg3 > leg1)) {
     //scalene
     if((leg1 !== leg2) && (leg2 !== leg3) && (leg1 !== leg3)) {
+      kind = " scalene";
       return true;
     }
     //isosceles
     else if (((leg1 === leg2) && (leg1 !== leg3)) || ((leg2 === leg3) && (leg1 !== leg2)) || ((leg1 === leg3) && (leg2 !== leg3))) {
+      kind = "n isosceles";
       return true;
     }
     //equilateral
     else if ((leg1 === leg2) && (leg1 === leg3)) {
+      kind = "n equilateral";
       return true;
     }
   } else {
     return false;
   }
-
 };
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    var leg1 = parseInt($("input#leg1").val());
+    var leg2 = parseInt($("input#leg2").val());
+    var leg3 = parseInt($("input#leg3").val());
+    var result = triangle(leg1, leg2, leg3);
+    // var kind = "";
+    if(!result) {
+      $(".kind").text(" not a triangle");
+    } else {
+      $(".kind").text(" a" + kind + " triangle");
+    }
+    $("#result").show();
+    event.preventDefault();
+  });
+});
